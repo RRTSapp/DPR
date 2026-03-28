85f9bba5fb82568843139bff89f5ed37c4de8e6d
RRTSapp <h2ran@yahoo.com>
2026-03-28 15:06:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -192,7 +192,5 @@ def preview():
         return jsonify({"error": str(e)}), 500
 
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
